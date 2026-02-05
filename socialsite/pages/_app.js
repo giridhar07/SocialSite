1617c7aa66014e86ae55a08a88c63abca0490a76
giridhar07 <giridharpodala14@gmail.com>
2026-02-05 15:51:42 +0000

    added postcard.jsx file and update code

--- socialsite/pages/_app.js
+++ socialsite/pages/_app.js
@@ -1,5 +1,19 @@
 import "@/styles/globals.css";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      user: "Giridhar",
+      text: "Hello! This is my first post.",
+      likes: 0,
+    },
+  ]);
+
+  return (
+    <>
+      <Component {...pageProps} posts={posts} setPosts={setPosts} />
+    </>
+  );
 }
--- socialsite/pages/index.js
+++ socialsite/pages/index.js
@@ -1,11 +1,23 @@
-import NavBar from "@/Components/NavBar";
+import Navbar from "../Components/NavBar";
+import PostCard from "../Components/PostCard";
 
+export default function Home({ posts, setPosts }) {
+  const handleLike = (id) => {
+    setPosts((prev) =>
+      prev.map((post) =>
+        post.id === id ? { ...post, likes: post.likes + 1 } : post
+      )
+    );
+  };
 
-export default function Home() {
   return (
     <>
-    <NavBar />
-    
+      <Navbar />
+      <main className="max-w-xl mx-auto p-6">
+        {posts.map((post) => (
+          <PostCard key={post.id} post={post} onLike={handleLike} />
+        ))}
+      </main>
     </>
   );
 }
